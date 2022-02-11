dccfdfa8c0372e088c95ef2a7a464236b482119e
ShevchenkoOleksii <84289820+ShevchenkoOleksii@users.noreply.github.com>
2022-02-12 01:15:00 +0200

    'add_auth_services'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: "AIzaSyCbhEOAw36rNZ0uAriKj7rf9Mdx4ca4p4M"
 };
